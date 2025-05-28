9ac9452049dc7c65c94461ab3cad569340e31f3a
Alessio Mora <mora.alessio20@gmail.com>
2025-05-28 15:17:01 +0200

    Update esercizi

--- docker-client-server-example/client.py
+++ docker-client-server-example/client.py
@@ -1,7 +1,9 @@
 import requests
- 
-url = "http://server:80/"
-# url = "http://20.241.134.170:80/"
+import time 
 
+url = "http://server:80/"
+# url = "http://0.0.0.0:80/"
+
+time.sleep(5)
 response = requests.get(url)
 print(response.json())
